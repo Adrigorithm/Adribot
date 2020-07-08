515abcedca821800de05924dd4738a41eb989e7c
Adriaan Waem <adriaan.waem@student.uhasselt.be>
2020-07-08 14:07:53 +0200

    Add project files.

---
+++ Adribot/Commands.cs
@@ -0,0 +1,77 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Adribot
+{
+    public class Commands
+    {
+        [Command("clear"), Description("Clears chat history"), RequirePermissions(Permissions.ManageMessages)]
+        public async Task ClearChat(CommandContext ctx, [Description("Amount of messages to be deleted.")] int amount = -1) {
+            var channel = ctx.Channel;
+
+            if(amount >= 0) {
+                bool old = false;
+                List<DiscordMessage> messages = new List<DiscordMessage>();
+                ulong lastId = ctx.Message.Id;
+
+                for(int i = amount; i > 0; i -= 100) {
+                    if(i >= 100) {
+                        messages.AddRange(await channel.GetMessagesAsync(100, lastId));
+                    } else {
+                        messages.AddRange(await channel.GetMessagesAsync(i, lastId));
+                    }
+
+                    if(DateTime.UtcNow > messages[messages.Count - 1].CreationTimestamp.UtcDateTime.AddDays(14)) {
+                        old = true;
+                    }
+
+                    await channel.DeleteMessagesAsync(messages);
+                }
+
+                // Remove command message
+                await channel.DeleteMessageAsync(ctx.Message, "Remove command");
+
+                if(old) {
+                    // (Partial) failure message
+                    var emojiWarning = DiscordEmoji.FromName(ctx.Client, ":warning:");
+                    await channel.SendMessageAsync($"{emojiWarning} Some messages have not been removed due to them being too old.\nConsider using `$clear` instead.");
+                } else {
+                    // Success notification
+                    var emojiSuccess = DiscordEmoji.FromName(ctx.Client, ":white_check_mark:");
+                    await channel.SendMessageAsync($"{emojiSuccess} Removed `{amount}` messages successfully!");
+                }
+            } else {
+                InteractivityModule interactivity = ctx.Client.GetInteractivityModule();
+
+                await ctx.RespondAsync("Reply `y` to remove all messages\nThis includes pinned messages!");
+
+                var approval = await interactivity.WaitForMessageAsync(m => m.Content.ToLower().Equals("y"), TimeSpan.FromSeconds(5));
+
+                if(approval.Message.Author.Equals(ctx.User)) {
+                    // Translate 0 to null
+                    int? tBitrate = channel.Bitrate;
+                    int? tUserLimit = channel.UserLimit;
+
+                    if(channel.Bitrate.Equals(0) && channel.UserLimit.Equals(0)) {
+                        tBitrate = null;
+                        tUserLimit = null;
+                    }
+
+                    // Reset channel
+                    var newChannel = await ctx.Guild.CreateChannelAsync(channel.Name, channel.Type, channel.Parent, tBitrate, tUserLimit, channel.PermissionOverwrites, "Force clear channel");
+                    await channel.DeleteAsync("Force clear channel");
+
+                    // Success notification
+                    var emojiSuccess = DiscordEmoji.FromName(ctx.Client, ":white_check_mark:");
+                    await newChannel.SendMessageAsync($"{emojiSuccess} Removed all messages successfully!");
+                }
+            }
+        }
+    }
+}
---
+++ Adribot/Program.cs
@@ -0,0 +1,82 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adribot
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static InteractivityModule interactivity;
+        static CommandsNextModule commands;
+
+        static async Task Main(string[] args) {
+            // Load config.json
+            string json = await GetConfigJson();
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            // Setup DClient
+            discord = new DiscordClient(new DiscordConfiguration {
+                Token = cfgjson.Token,
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            // Attach Interactivity module
+            interactivity = discord.UseInteractivity(new InteractivityConfiguration {
+                PaginationBehaviour = TimeoutBehaviour.Ignore,
+                PaginationTimeout = TimeSpan.FromMinutes(5),
+                Timeout = TimeSpan.FromMinutes(1)
+            });
+
+            // Attach CommandsNext module
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration {
+                StringPrefix = cfgjson.CommandPrefix
+            });
+
+            // Register Commands
+            commands.RegisterCommands<Commands>();
+
+            // Event handlers
+            discord.MessageCreated += MessageCreated;
+
+            await discord.ConnectAsync();
+
+            // Infinite task
+            await Task.Delay(-1);
+        }
+
+        static async Task MessageCreated(MessageCreateEventArgs e) {
+            foreach(var user in e.MentionedUsers) {
+                if(user.Id.Equals(135081249017430016) && e.Author.Id != 608275633218519060) {
+                    await e.Message.RespondAsync($"Don't tag Adri, bad! {e.Author.Mention}\nhttps://imgur.com/F7WNiqc");
+                }
+            }
+        }
+
+        static async Task<string> GetConfigJson() {
+            using(var fs = File.OpenRead("config.json")) {
+                using(var sr = new StreamReader(fs, new UTF8Encoding(false))) {
+                    return await sr.ReadToEndAsync();
+                }
+            }
+        }
+
+    }
+
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+    }
+}
