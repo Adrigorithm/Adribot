a4a1d8a5e259d12ed2ea578357ff9883b0e04d6e
Adriaan Waem <2ndalgorithm@gmail.com>
2024-07-09 08:54:10 +0200

    admin commands are done as well :3

--- Adribot.App/src/commands/moderation/AdminCommands.cs
+++ Adribot.App/src/commands/moderation/AdminCommands.cs
@@ -1,53 +1,55 @@
 using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Adribot.src.constants.enums;
 using Adribot.src.extensions;
 using Adribot.src.helpers;
 using Adribot.src.services;
-using DSharpPlus.Entities;
-using DSharpPlus.SlashCommands;
-using DSharpPlus.SlashCommands.Attributes;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
 
 namespace Adribot.src.commands.moderation;
 
-public class AdminCommands(InfractionService _infractionService) : ApplicationCommandModule
+public class AdminCommands(InfractionService _infractionService) : InteractionModuleBase
 {
-    [SlashCommand("Clear", "Deletes given amount of messages")]
-    [RequirePermissionOrDev(135081249017430016, DiscordPermissions.ManageMessages)]
-    public async Task DeleteMessagesAsync(InteractionContext ctx, [Option("Amount", "Amount of messages to delete"), Minimum(1), Maximum(100)] long amount)
+    [SlashCommand("clear", "Deletes given amount of messages")]
+    [RequirePermissionOrUser(135081249017430016, ChannelPermission.ManageMessages)]
+    public async Task DeleteMessagesAsync(InteractionContext ctx, [Summary("Amount", "Amount of messages to delete"), MinValue(1), MaxValue(100)] short amount)
     {
-        var deletedMessages = await ctx.Channel.DeleteMessagesAsync(ctx.Channel.GetMessagesAsync((int)amount));
+        IAsyncEnumerable<IReadOnlyCollection<IMessage>> messages = ctx.Channel.GetMessagesAsync(amount);
+        IEnumerable<IMessage> messagesFlattened = await messages.FlattenAsync();
 
-        var oldMessages = amount - deletedMessages;
-        StringBuilder confirmMessage = new();
+        await (ctx.Channel as ITextChannel).DeleteMessagesAsync(messagesFlattened);
 
-        if (oldMessages > 0)
-            confirmMessage.AppendLine($"Ink too dry! {oldMessages} Message{(oldMessages > 1 ? "s" : "")} could not be deleted.");
-        confirmMessage.AppendLine($"Deleted {deletedMessages} Message{(deletedMessages > 1 ? "s" : "")}.");
+        IMessage? oldMessage = await ctx.Channel.GetMessageAsync(messagesFlattened.Last().Id);
+        var confirmMessage = oldMessage is not null
+            ? $"Ink too dry! Some messages could not be deleted."
+            : $"Deleted {messagesFlattened.Count()} Message{(messagesFlattened.Count() > 1 ? "s" : "")}.";
 
-        await ctx.CreateResponseAsync(DiscordInteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(confirmMessage.ToString()).AsEphemeral(true));
+        await RespondAsync(confirmMessage.ToString(), ephemeral: true);
     }
 
-    [SlashCommand("Mute", "Mutes member using a Timeout")]
-    [SlashRequirePermissions(DiscordPermissions.MuteMembers)]
-    public async Task MuteMemberAsync(InteractionContext ctx, [Option("Member", "Member to mute")] DiscordUser member, [Option("Unit", "The duration multiplied by the factor parameter")] TimeSpanType type = TimeSpanType.Minutes, [Option("Factor", "The amound of specified time units."), Minimum(1)] long factor = 3, [Option("Reason", "The reason for this infraction")] string reason = "")
+    [SlashCommand("mute", "Mutes member using a Timeout")]
+    [RequireUserPermission(GuildPermission.MuteMembers)]
+    public async Task MuteMemberAsync([Summary("Member", "Member to mute")] SocketGuildUser member, [Summary("Unit", "The duration multiplied by the factor parameter")] TimeSpanType type = TimeSpanType.Minutes, [Summary("Factor", "The amound of specified time units."), MinValue(1)] int factor = 3, [Summary("Reason", "The reason for this infraction")] string? reason = null)
     {
         DateTimeOffset now = DateTimeOffset.UtcNow;
-        DateTimeOffset endDate = type.ToEndDate((int)factor, now);
+        DateTimeOffset endDate = type.ToEndDate(factor, now);
 
-        await ((DiscordMember)member).TimeoutAsync(endDate, reason);
+        await member.SetTimeOutAsync(endDate - now);
     }
 
-    [SlashCommand("Ban", "Bans members")]
-    [SlashRequirePermissions(DiscordPermissions.BanMembers)]
-    public async Task BanMemberAsync(InteractionContext ctx, [Option("Member", "Member to ban")] DiscordUser member, [Option("Unit", "The duration multiplied by the factor parameter")] TimeSpanType type = TimeSpanType.Months, [Option("Factor", "The amound of specified time units."), Minimum(1)] long factor = 1, [Option("Messages", "Anount of messages by this user to delete")] long deleteMessages = 0, [Option("Reason", "The reason for this infraction")] string reason = "")
+    [SlashCommand("ban", "Bans members")]
+    [RequireUserPermission(GuildPermission.BanMembers)]
+    public async Task BanMemberAsync(InteractionContext ctx, [Summary("Member", "Member to ban")] SocketGuildUser member, [Summary("Unit", "The duration multiplied by the factor parameter")] TimeSpanType type = TimeSpanType.Months, [Summary("Factor", "The amound of specified time units."), MinValue(1)] int factor = 1, [Summary("Messages", "Anount of messages by this user to delete")] int deleteMessages = 0, [Summary("Reason", "The reason for this infraction")] string? reason = null)
     {
         DateTimeOffset now = DateTimeOffset.UtcNow;
-        DateTimeOffset endDate = type.ToEndDate((int)factor, now);
+        DateTimeOffset endDate = type.ToEndDate(factor, now);
 
-        _infractionService.AddInfraction(ctx.Guild.Id, ctx.Member.Id, endDate, InfractionType.Ban, reason);
+        _infractionService.AddInfraction(ctx.Guild.Id, member.Id, endDate, InfractionType.Ban, reason);
 
-        await ((DiscordMember)member).BanAsync(Convert.ToInt16(deleteMessages), reason);
+        await member.BanAsync(deleteMessages, reason);
     }
 }
