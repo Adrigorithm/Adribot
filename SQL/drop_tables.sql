e9681a679d79b9f7043e05c4bd8125526076163e
Adriaan Waem <2ndalgorithm@gmail.com>
2024-07-29 15:38:29 +0200

    fixed healthcheck script

---
+++ SQL/drop_tables.sql
@@ -0,0 +1,9 @@
+DROP TABLE [dbo].[Tags]
+DROP TABLE [dbo].[Reminders]
+DROP TABLE [dbo].[Events]
+DROP TABLE [dbo].[IcsCalendars]
+DROP TABLE [dbo].[Infractions]
+DROP TABLE [dbo].[DMembers]
+DROP TABLE [dbo].[DGuilds]
+DROP TABLE [dbo].[__EFMigrationsHistory]
+GO
