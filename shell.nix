f96211b20ce9347d53b52c76cfb040b68331f9e9
Adriaan Waem <2ndalgorithm@gmail.com>
2024-10-09 09:43:36 +0200

    IT'S WIRED NOW - I mean nixxed :3

--- shell.nix
+++ shell.nix
@@ -1,4 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ dotnetCorePackages.sdk_9_0 ];
+
+let
+  lib = pkgs.lib;
+  vscodeExtensions = [
+    "ms-dotnettools.csdevkit"
+  ];
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [ dotnetCorePackages.sdk_8_0_2xx dotnetCorePackages.sdk_9_0 ];
+  
+  shellHook = ''
+    for ext in ${lib.concatStringsSep " " vscodeExtensions}; do
+      code --install-extension $ext || true
+    done
+  '';
 }
