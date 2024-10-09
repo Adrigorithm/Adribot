c58a8816e13cd3dbf82b6b4ac1a77e1b2f806039
Adriaan Waem <2ndalgorithm@gmail.com>
2024-10-09 12:42:18 +0200

    I guess we doing Rider now

--- shell.nix
+++ shell.nix
@@ -1,17 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  lib = pkgs.lib;
-  vscodeExtensions = [
-    "ms-dotnettools.csdevkit"
-  ];
-in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ dotnetCorePackages.sdk_8_0_2xx dotnetCorePackages.sdk_9_0 ];
-  
-  shellHook = ''
-    for ext in ${lib.concatStringsSep " " vscodeExtensions}; do
-      code --install-extension $ext || true
-    done
-  '';
+  nativeBuildInputs = with pkgs.buildPackages; [ dotnetCorePackages.sdk_9_0 jetbrains.rider ];
 }
