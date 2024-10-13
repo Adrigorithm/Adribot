e0131dabe80d9b5fb2932ce48e5dabab16397058
Adriaan Waem <2ndalgorithm@gmail.com>
2024-10-14 00:08:25 +0200

    monitoring service done I think

--- Adribot.App/src/services/StatisticsService.cs
+++ Adribot.App/src/services/StatisticsService.cs
@@ -1,38 +1,54 @@
 using System.Collections.Frozen;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Adribot.src.constants.enums;
-using Adribot.src.services.providers;
+using Adribot.services.providers;
 using Discord.WebSocket;
 
-namespace Adribot.src.services;
+namespace Adribot.services;
 
 public class StatisticsService(DiscordClientProvider clientProvider)
 {
     /// <summary>
-    /// Get all cached registered commands from the current user
+    /// Get all cached registered commands in a specific guild
     /// </summary>
     /// <param name="guild">Guild to search commands in</param>
-    /// <returns>A dictionary where the key indicates the availability of the command. Returns null if no commands were found in the guild.</returns>
-    public async Task<FrozenDictionary<CommandScope, SocketApplicationCommand>?> GetCommandsByGuildAsync(SocketGuild guild)
+    /// <returns>A dictionary where the key is the command name. Returns null if no commands were found in the guild.</returns>
+    public async Task<FrozenDictionary<string, SocketApplicationCommand>> GetGuildCommandsAsync(SocketGuild guild)
     {
         IReadOnlyCollection<SocketApplicationCommand> commands = await guild.GetApplicationCommandsAsync();
 
         if (commands.Count == 0)
-            return null;
+            return FrozenDictionary<string, SocketApplicationCommand>.Empty;
 
-        return commands.ToFrozenDictionary(c => c.IsGlobalCommand
-                ? CommandScope.Global
-                : CommandScope.Guild
-            , c => c);
+        return commands.ToFrozenDictionary(c => c.Name, c => c);
     }
 
     /// <summary>
-    /// Get all cached registered commands from the current user
+    /// Get all cached registered commands in a specific guild
     /// </summary>
     /// <param name="guildId">Id of the guild to search commands in</param>
-    /// <returns>A dictionary where the key indicates the availability of the command. Returns null if no commands were found in the guild.</returns>
-    public async Task<FrozenDictionary<CommandScope, SocketApplicationCommand>?> GetCommandsByGuildAsync(ulong guildId)
-        => await GetCommandsByGuildAsync(clientProvider.Client.GetGuild(guildId));
+    /// <returns>A dictionary where the key is the command name. Returns null if no commands were found in the guild.</returns>
+    public async Task<FrozenDictionary<string, SocketApplicationCommand>> GetGuildCommandsAsync(ulong guildId)
+        => await GetGuildCommandsAsync(clientProvider.Client.GetGuild(guildId));
+
+    public async Task<Dictionary<ulong?, FrozenDictionary<string, SocketApplicationCommand>>> GetAllCommandsAsync()
+    {
+        var commands = new Dictionary<ulong?, FrozenDictionary<string, SocketApplicationCommand>?> { [null!] = await GetGlobalCommandsAsync() };
+
+        foreach (SocketGuild guild in clientProvider.Client.Guilds)
+        {
+            FrozenDictionary<string, SocketApplicationCommand> guildCommands = await GetGuildCommandsAsync(guild);
+
+            if (guildCommands.Count > 0)
+                commands[guild.Id] = guildCommands;
+        }
+        
+        return commands;
+    }
+
+    public async Task<FrozenDictionary<string, SocketApplicationCommand>> GetGlobalCommandsAsync()
+    {
+        IReadOnlyCollection<SocketApplicationCommand> globalCommands = await clientProvider.Client.GetGlobalApplicationCommandsAsync();
+        return globalCommands.ToFrozenDictionary(c => c.Name, c => c);
+    }
 }
