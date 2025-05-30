3d63a1d7dc235392f363f7d6e16de08e3ec33cb0
Adriaan Waem <2ndalgorithm@gmail.com>
2025-05-30 16:54:12 +0200

    updated packages

--- zed.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ dotnetCorePackages.sdk_9_0 zed-editor ];
-  
-  DOTNET_ROOT = "/nix/store/xrbswlskla2qi3hqf2j2543k1qwq10l0-dotnet-sdk-9.0.100-rc.1.24452.12";
-}
