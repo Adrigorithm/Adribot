0f1d1a1464edb5da2fee13f8eadf31485daac582
Adriaan Waem <2ndalgorithm@gmail.com>
2025-06-26 13:40:23 +0200

    fu rider stop whining

--- Adribot.App/src/entities/discord/DGuild.cs
+++ Adribot.App/src/entities/discord/DGuild.cs
@@ -44,7 +44,7 @@ public class DGuild : IComparable, IDataStructure
     {
         unchecked
         {
-            return (GuildId.GetHashCode() * 397) ^ Members.GetHashCode();
+            return GuildId.GetHashCode() * 397 ^ Members.GetHashCode();
         }
     }
 
