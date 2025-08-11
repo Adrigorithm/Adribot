4e37945a447ccf50f62de23f95a98e10a0953822
Adriaan Waem <2ndalgorithm@gmail.com>
2025-08-11 13:00:19 +0200

    missed location types ( why are these never grouped :( )

---
+++ Adribot.App/src/entities/fun/pokemon/VersionEncounterDetail.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Adribot.Entities.fun.pokemon;
+
+public record VersionEncounterDetail(
+    [property: JsonPropertyName("version")] NamedApiResource Version,
+    
+    // The total percentage of all encounter potential.
+    [property: JsonPropertyName("max_chance")] int MaxChance,
+    
+    [property: JsonPropertyName("encounter_details")] List<Encounter> EncounterDetails
+);
