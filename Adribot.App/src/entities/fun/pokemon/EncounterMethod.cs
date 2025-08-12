8b0c4c0b143bce3a6ba129c9dbc9878fb401dbcb
Adriaan Waem <2ndalgorithm@gmail.com>
2025-08-12 10:14:57 +0200

    encounter entities

---
+++ Adribot.App/src/entities/fun/pokemon/EncounterMethod.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Adribot.Entities.fun.pokemon;
+
+public record EncounterMethod(
+    [property: JsonPropertyName("id")] int Id,
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("order")] int Order,
+    
+    // The name of this resource listed in different languages.
+    [property: JsonPropertyName("names")] List<Name> Names
+);
