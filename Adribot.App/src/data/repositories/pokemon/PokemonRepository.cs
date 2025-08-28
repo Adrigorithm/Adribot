2c640af4725037d855e0c641cec73681a52c0397
Adriaan Waem <2ndalgorithm@gmail.com>
2025-08-28 08:38:37 +0200

    api get methods :3

--- Adribot.App/src/data/repositories/pokemon/PokemonRepository.cs
+++ Adribot.App/src/data/repositories/pokemon/PokemonRepository.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Adribot.entities.fun.pokemon;
 using Microsoft.EntityFrameworkCore;
 
 namespace Adribot.data.repositories.pokemon;
@@ -5,5 +7,19 @@ namespace Adribot.data.repositories.pokemon;
 public sealed class PokemonRepository(IDbContextFactory<AdribotContext> botContextFactory)
     : BaseRepository(botContextFactory)
 {
-    
+    public void Add(object pokemonObject)
+    {
+        using AdribotContext dbContext = CreateDbContext();
+        
+        dbContext.Add(pokemonObject);
+        dbContext.SaveChanges();
+    }
+
+    public void AddRange(IEnumerable<object> pokemonObjects)
+    {
+        using AdribotContext dbContext = CreateDbContext();
+        
+        dbContext.AddRange(pokemonObjects);
+        dbContext.SaveChanges();
+    }
 }
--- Adribot.App/src/services/PokemonService.cs
+++ Adribot.App/src/services/PokemonService.cs
@@ -1,5 +1,48 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Adribot.constants.enums;
+using Adribot.data.repositories.pokemon;
+using Adribot.entities.fun.pokemon;
+using Adribot.helpers;
+
 namespace Adribot.services;
 
-public class PokemonService
+public class PokemonService(PokemonRepository pokemonRepository, IHttpClientFactory httpClientFactory)
 {
+    public async Task GetEverythingAsync()
+    {
+        await GetAllBerriesAsync();
+        await GetAllBerryFirmnessesAsync();
+    }
+
+    private (string? endpoint, string? error) GetListEndpointString(PokemonEndpoint pokemonEndpoint) =>
+        PokemonEndpointStringGenerator.GetEndpointString(new EndpointStringConfiguration(pokemonEndpoint, true, null, null));
+    
+    public async Task GetAllBerriesAsync()
+    {
+        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.Berry);
+
+        if (endpointString.error is not null)
+            return;
+
+        List<Berry>? berries = await JsonSerializer.DeserializeAsync<List<Berry>>(
+            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
+        
+        pokemonRepository.AddRange(berries);
+    }
+    
+    public async Task GetAllBerryFirmnessesAsync()
+    {
+        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.BerryFirmness);
+
+        if (endpointString.error is not null)
+            return;
+
+        List<BerryFirmness>? berryFirmnesses = await JsonSerializer.DeserializeAsync<List<BerryFirmness>>(
+            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
+        
+        pokemonRepository.AddRange(berryFirmnesses);
+    }
 }
