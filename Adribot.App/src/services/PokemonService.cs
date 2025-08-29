190c3826b9609b80813943f007e1aabd266fe82a
Adrigorithm <2ndalgorithm@gmail.com>
2025-08-29 09:39:02 +0200

    more endpoints :3

--- Adribot.App/src/services/PokemonService.cs
+++ Adribot.App/src/services/PokemonService.cs
@@ -63,147 +63,147 @@ public class PokemonService(PokemonRepository pokemonRepository, IHttpClientFact
     public async Task<List<Berry>?> GetAllBerriesAsync(bool shouldAvoidCache = false) =>
         await GetAllAsync<Berry>(shouldAvoidCache, PokemonEndpoint.Berry, true);
     
-    public async Task<List<BerryFirmness>?> GetAllBerryFirmnessesAsync(bool shouldAvoidCache = false)
-    {
-        if (!shouldAvoidCache)
-            return pokemonRepository.GetAll<BerryFirmness>();
-        
-        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.BerryFirmness);
-
-        if (endpointString.error is not null)
-            return null;
-
-        List<BerryFirmness>? berryFirmnesses = await JsonSerializer.DeserializeAsync<List<BerryFirmness>>(
-            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
-        
-        return pokemonRepository.AddRange(berryFirmnesses);
-    }
+    public async Task<List<BerryFirmness>?> GetAllBerryFirmnessesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<BerryFirmness>(shouldAvoidCache, PokemonEndpoint.BerryFirmness, true);
     
-    public async Task<List<BerryFlavour>?> GetAllBerryFlavoursAsync(bool shouldAvoidCache = false)
-    {
-        if (!shouldAvoidCache)
-            return pokemonRepository.GetAll<BerryFlavour>();
-        
-        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.BerryFlavour);
-
-        if (endpointString.error is not null)
-            return null;
-
-        List<BerryFlavour>? berryFlavours = await JsonSerializer.DeserializeAsync<List<BerryFlavour>>(
-            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
-        
-        return pokemonRepository.AddRange(berryFlavours);
-    }
+    public async Task<List<BerryFlavour>?> GetAllBerryFlavoursAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<BerryFlavour>(shouldAvoidCache, PokemonEndpoint.BerryFlavour, true);
     
-    public async Task<List<ContestType>?> GetAllContestTypesAsync(bool shouldAvoidCache = false)
-    {
-        if (!shouldAvoidCache)
-            return pokemonRepository.GetAll<ContestType>();
-        
-        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.ContestType);
-
-        if (endpointString.error is not null)
-            return null;
-
-        List<ContestType>? contestTypes = await JsonSerializer.DeserializeAsync<List<ContestType>>(
-            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
-        
-        return pokemonRepository.AddRange(contestTypes);
-    }
+    public async Task<List<ContestType>?> GetAllContestTypesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<ContestType>(shouldAvoidCache, PokemonEndpoint.ContestType, true);
     
-    public async Task<List<ContestEffect>?> GetAllContestEffectsAsync(bool shouldAvoidCache = false)
-    {
-        if (!shouldAvoidCache)
-            return pokemonRepository.GetAll<ContestEffect>();
-        
-        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.ContestEffect);
-
-        if (endpointString.error is not null)
-            return null;
-
-        List<ContestEffect>? contestEffects = await JsonSerializer.DeserializeAsync<List<ContestEffect>>(
-            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
-        
-        return pokemonRepository.AddRange(contestEffects);
-    }
+    public async Task<List<ContestEffect>?> GetAllContestEffectsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<ContestEffect>(shouldAvoidCache, PokemonEndpoint.ContestEffect, false);
     
-    public async Task<List<SuperContestEffect>?> GetAllSuperContestEffectsAsync(bool shouldAvoidCache = false)
-    {
-        if (!shouldAvoidCache)
-            return pokemonRepository.GetAll<SuperContestEffect>();
-        
-        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.SuperContestEffect);
-
-        if (endpointString.error is not null)
-            return null;
-
-        List<SuperContestEffect>? superContestEffect = await JsonSerializer.DeserializeAsync<List<SuperContestEffect>>(
-            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
-        
-        return pokemonRepository.AddRange(superContestEffect);
-    }
+    public async Task<List<SuperContestEffect>?> GetAllSuperContestEffectsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<SuperContestEffect>(shouldAvoidCache, PokemonEndpoint.SuperContestEffect, false);
     
-    public async Task<List<EncounterMethod>?> GetAllEncounterMethodsAsync(bool shouldAvoidCache = false)
-    {
-        if (!shouldAvoidCache)
-            return pokemonRepository.GetAll<EncounterMethod>();
-        
-        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.EncounterMethod);
-
-        if (endpointString.error is not null)
-            return null;
-
-        List<EncounterMethod>? encounterMethod = await JsonSerializer.DeserializeAsync<List<EncounterMethod>>(
-            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
-        
-        return pokemonRepository.AddRange(encounterMethod);
-    }
+    public async Task<List<EncounterMethod>?> GetAllEncounterMethodsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<EncounterMethod>(shouldAvoidCache, PokemonEndpoint.EncounterMethod, true);
     
-    public async Task<List<EncounterCondition>?> GetAllEncounterConditionsAsync(bool shouldAvoidCache = false)
-    {
-        if (!shouldAvoidCache)
-            return pokemonRepository.GetAll<EncounterCondition>();
-        
-        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.EncounterCondition);
-
-        if (endpointString.error is not null)
-            return null;
-
-        List<EncounterCondition>? encounterCondition = await JsonSerializer.DeserializeAsync<List<EncounterCondition>>(
-            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
-        
-        return pokemonRepository.AddRange(encounterCondition);
-    }
+    public async Task<List<EncounterCondition>?> GetAllEncounterConditionsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<EncounterCondition>(shouldAvoidCache, PokemonEndpoint.EncounterCondition, true);
     
-    public async Task<List<EncounterConditionValue>?> GetAllEncounterConditionValuesAsync(bool shouldAvoidCache = false)
-    {
-        if (!shouldAvoidCache)
-            return pokemonRepository.GetAll<EncounterConditionValue>();
-        
-        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.EncounterConditionValue);
-
-        if (endpointString.error is not null)
-            return null;
-
-        List<EncounterConditionValue>? encounterConditionValue = await JsonSerializer.DeserializeAsync<List<EncounterConditionValue>>(
-            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
-        
-        return pokemonRepository.AddRange(encounterConditionValue);
-    }
+    public async Task<List<EncounterConditionValue>?> GetAllEncounterConditionValuesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<EncounterConditionValue>(shouldAvoidCache, PokemonEndpoint.EncounterConditionValue, true);
     
-    public async Task<List<EvolutionChain>?> GetAllEvolutionChainsAsync(bool shouldAvoidCache = false)
-    {
-        if (!shouldAvoidCache)
-            return pokemonRepository.GetAll<EvolutionChain>();
-        
-        (string? endpoint, string? error) endpointString = GetListEndpointString(PokemonEndpoint.EvolutionChain);
-
-        if (endpointString.error is not null)
-            return null;
-
-        List<EvolutionChain>? evolutionChain = await JsonSerializer.DeserializeAsync<List<EvolutionChain>>(
-            await httpClientFactory.CreateClient().GetStreamAsync(endpointString.endpoint));
-        
-        return pokemonRepository.AddRange(evolutionChain);
-    }
+    public async Task<List<EvolutionChain>?> GetAllEvolutionChainsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<EvolutionChain>(shouldAvoidCache, PokemonEndpoint.EvolutionChain, false);
+    
+    public async Task<List<EvolutionTrigger>?> GetAllEvolutionTriggersAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<EvolutionTrigger>(shouldAvoidCache, PokemonEndpoint.EvolutionTrigger, true);
+    
+    public async Task<List<Generation>?> GetAllGenerationsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Generation>(shouldAvoidCache, PokemonEndpoint.Generation, true);
+    
+    public async Task<List<Pokedex>?> GetAllPokedexesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Pokedex>(shouldAvoidCache, PokemonEndpoint.Pokedex, true);
+    
+    public async Task<List<Version>?> GetAllVersionsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Version>(shouldAvoidCache, PokemonEndpoint.Version, true);
+    
+    public async Task<List<VersionGroup>?> GetAllVersionGroupsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<VersionGroup>(shouldAvoidCache, PokemonEndpoint.VersionGroup, true);
+    
+    public async Task<List<Item>?> GetAllItemsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Item>(shouldAvoidCache, PokemonEndpoint.Item, true);
+    
+    public async Task<List<ItemAttribute>?> GetAllItemAttributesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<ItemAttribute>(shouldAvoidCache, PokemonEndpoint.ItemAttribute, true);
+    
+    public async Task<List<ItemCategory>?> GetAllItemCategoriesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<ItemCategory>(shouldAvoidCache, PokemonEndpoint.ItemCategory, true);
+    
+    public async Task<List<ItemFlingEffect>?> GetAllItemFlingEffectsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<ItemFlingEffect>(shouldAvoidCache, PokemonEndpoint.ItemFlingEffect, true);
+    
+    public async Task<List<ItemPocket>?> GetAllItemPocketsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<ItemPocket>(shouldAvoidCache, PokemonEndpoint.ItemPocket, true);
+    
+    public async Task<List<Location>?> GetAllLocationsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Location>(shouldAvoidCache, PokemonEndpoint.Location, true);
+    
+    public async Task<List<LocationArea>?> GetAllLocationAreasAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<LocationArea>(shouldAvoidCache, PokemonEndpoint.LocationArea, true);
+    
+    public async Task<List<PalParkArea>?> GetAllPalParkAreasAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<PalParkArea>(shouldAvoidCache, PokemonEndpoint.PalParkArea, true);
+    
+    public async Task<List<Region>?> GetAllRegionsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Region>(shouldAvoidCache, PokemonEndpoint.Region, true);
+    
+    public async Task<List<Machine>?> GetAllMachinesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Machine>(shouldAvoidCache, PokemonEndpoint.Machine, false);
+    
+    public async Task<List<Move>?> GetAllMovesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Move>(shouldAvoidCache, PokemonEndpoint.Move, true);
+    
+    public async Task<List<MoveAilment>?> GetAllMoveAilmentsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<MoveAilment>(shouldAvoidCache, PokemonEndpoint.MoveAilment, true);
+    
+    public async Task<List<MoveBattleStyle>?> GetAllMoveBattleStylesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<MoveBattleStyle>(shouldAvoidCache, PokemonEndpoint.MoveBattleStyle, true);
+    
+    public async Task<List<MoveCategory>?> GetAllMoveCategoriesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<MoveCategory>(shouldAvoidCache, PokemonEndpoint.MoveCategory, true);
+    
+    public async Task<List<MoveDamageClass>?> GetAllMoveDamageClassesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<MoveDamageClass>(shouldAvoidCache, PokemonEndpoint.MoveDamageClass, true);
+    
+    public async Task<List<MoveLearnMethod>?> GetAllMoveLearnMethodsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<MoveLearnMethod>(shouldAvoidCache, PokemonEndpoint.MoveLearnMethod, true);
+    
+    public async Task<List<MoveTarget>?> GetAllMoveTargetsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<MoveTarget>(shouldAvoidCache, PokemonEndpoint.MoveTarget, true);
+    
+    public async Task<List<Ability>?> GetAllAbilitiesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Ability>(shouldAvoidCache, PokemonEndpoint.Ability, true);
+    
+    public async Task<List<Characteristic>?> GetAllCharacteristicsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Characteristic>(shouldAvoidCache, PokemonEndpoint.Characteristic, false);
+    
+    public async Task<List<EggGroup>?> GetAllEggGroupsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<EggGroup>(shouldAvoidCache, PokemonEndpoint.EggGroup, true);
+    
+    public async Task<List<Gender>?> GetAllGenusAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Gender>(shouldAvoidCache, PokemonEndpoint.Gender, true);
+    
+    public async Task<List<GrowthRate>?> GetAllGrowthRatesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<GrowthRate>(shouldAvoidCache, PokemonEndpoint.GrowthRate, true);
+    
+    public async Task<List<Nature>?> GetAllNaturesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Nature>(shouldAvoidCache, PokemonEndpoint.Nature, true);
+    
+    public async Task<List<PokeathlonStat>?> GetAllPokeathlonStatsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<PokeathlonStat>(shouldAvoidCache, PokemonEndpoint.PokeathlonStat, true);
+    
+    public async Task<List<Pokemon>?> GetAllPokemonAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Pokemon>(shouldAvoidCache, PokemonEndpoint.Pokemon, true);
+    
+    public async Task<List<LocationAreaEncounter>?> GetAllPokemonLocationAreasAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<LocationAreaEncounter>(shouldAvoidCache, PokemonEndpoint.PokemonLocationArea, true);
+    
+    public async Task<List<PokemonColour>?> GetAllPokemonColoursAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<PokemonColour>(shouldAvoidCache, PokemonEndpoint.PokemonColour, true);
+    
+    public async Task<List<PokemonForm>?> GetAllPokemonFormsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<PokemonForm>(shouldAvoidCache, PokemonEndpoint.PokemonForm, true);
+    
+    public async Task<List<PokemonHabitat>?> GetAllPokemonHabitatsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<PokemonHabitat>(shouldAvoidCache, PokemonEndpoint.PokemonHabitat, true);
+    
+    public async Task<List<PokemonShape>?> GetAllPokemonShapesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<PokemonShape>(shouldAvoidCache, PokemonEndpoint.PokemonShape, true);
+    
+    public async Task<List<PokemonSpecies>?> GetAllPokemonSpeciesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<PokemonSpecies>(shouldAvoidCache, PokemonEndpoint.PokemonSpecies, true);
+    
+    public async Task<List<Stat>?> GetAllStatsAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Stat>(shouldAvoidCache, PokemonEndpoint.Stat, true);
+    
+    public async Task<List<Type>?> GetAllTypesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Type>(shouldAvoidCache, PokemonEndpoint.Type, true);
+    
+    public async Task<List<Language>?> GetAllLanguagesAsync(bool shouldAvoidCache = false) =>
+        await GetAllAsync<Language>(shouldAvoidCache, PokemonEndpoint.Language, true);
 }
